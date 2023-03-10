32a57cf8927b36f19879793fa695f4086874e18b
Gabriela Heimer <gvh@cin.ufpe.br>
2023-03-10 08:05:39 -0300

    add about page

--- app/screens/Settings/About/index.tsx
+++ app/screens/Settings/About/index.tsx
@@ -1,9 +1,45 @@
-import { View, Text } from "react-native";
+import { openURL } from "expo-linking";
+import * as Application from "expo-application";
+import { StyleSheet } from "react-native";
+import { Caption, Divider, List } from "react-native-paper";
 
 export default function About() {
-    return (
-        <View>
-            <Text>About!</Text>
-        </View>
-    );
+  const openAboutUs = () => {
+    try {
+      openURL("https://www.heylinda.app/about");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      <List.Item
+        title="Application Version"
+        right={() => (
+          <Caption style={styles.caption}>
+            {Application.nativeApplicationVersion}
+          </Caption>
+        )}
+      />
+      <Divider />
+      <List.Item
+        title="Build Version"
+        right={() => (
+          <Caption style={styles.caption}>
+            {Application.nativeBuildVersion}
+          </Caption>
+        )}
+      />
+      <Divider />
+      <List.Item title="About Us" onPress={openAboutUs} />
+      <Divider />
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  caption: {
+    marginTop: 5,
+  },
+});
