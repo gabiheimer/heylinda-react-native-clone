13e5f25c5b90bd5a1ec65504fe03d8cc8c5ad979
Gabriela Heimer <gvh@cin.ufpe.br>
2023-03-09 21:54:01 -0300

    add stats screen

---
+++ app/screens/Stats/Calendar/index.tsx
@@ -0,0 +1,70 @@
+import * as React from "react";
+import { StyleSheet } from "react-native";
+import dayjs from "dayjs";
+
+import { Calendar as DefaultCalendar } from "react-native-calendars";
+import Colors from "../../../constants/Colors";
+
+interface Props {
+  markedDates: string[];
+}
+
+export default function Calendar({ markedDates }: Props) {
+  const white = Colors.white;
+  const primary = Colors.primary;
+  const textColor = Colors.text;
+
+  const formattedMarkedDates = () => {
+    const today = dayjs().format("YYYY-MM-DD");
+    let result = { [today]: { marked: true } };
+    markedDates.forEach((date) => {
+      result = {
+        ...result,
+        [date]: {
+          marked: true,
+        },
+      };
+    });
+
+    return result;
+  };
+
+  return (
+    <DefaultCalendar
+      style={styles.calendar}
+      markedDates={formattedMarkedDates()}
+      theme={{
+        backgroundColor: white,
+        calendarBackground: white,
+        textSectionTitleColor: "#b6c1cd",
+        selectedDayBackgroundColor: primary,
+        selectedDayTextColor: white,
+        todayTextColor: primary,
+        dayTextColor: textColor,
+        textDisabledColor: "#d9e1e8",
+        dotColor: primary,
+        selectedDotColor: white,
+        arrowColor: textColor,
+        monthTextColor: textColor,
+        indicatorColor: "blue",
+        textDayFontWeight: "300",
+        textMonthFontWeight: "bold",
+        textDayHeaderFontWeight: "300",
+        textDayFontSize: 16,
+        textMonthFontSize: 16,
+        textDayHeaderFontSize: 16,
+      }}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  calendar: {
+    marginRight: 14,
+    marginBottom: 30,
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+});
--- app/screens/Stats/index.tsx
+++ app/screens/Stats/index.tsx
@@ -1,9 +1,126 @@
-import { View, Text } from "react-native";
+import { useEffect, useState } from "react";
+import { Card, Paragraph, Title } from "react-native-paper";
+import { ScrollView, StyleSheet, View } from "react-native";
+import Screen from "../../components/Screen";
+import Colors from "../../constants/Colors";
+import quotes from "../../data/quotes";
+import { AntDesign as Icon } from "@expo/vector-icons";
+import Calendar from "./Calendar";
 
-export default function Stats() {
-    return (
-        <View>
-            <Text>Stats!</Text>
+const getQuote = (): { quote: string; author: string } => {
+  const max = quotes.length - 1;
+  const r = Math.floor(Math.random() * max);
+  const item = quotes[r];
+  const quote = item.text;
+  const author = item.author;
+
+  return {
+    quote,
+    author,
+  };
+};
+
+export default function StatsScreen() {
+  const [markedDates, setMarkedDates] = useState([]);
+  const [totalSessions, setTotalSessions] = useState(0);
+  const [listenedStat, setListenedStat] = useState(0);
+  const [streak, setStreak] = useState(0);
+  const primary = Colors.primary;
+  const quote = getQuote();
+
+  async function getCalendarData(): Promise<void> {
+    // TODO: get data
+  }
+
+  useEffect(() => {
+    getCalendarData();
+  }, []);
+
+  return (
+    <>
+      <Screen scroll>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          style={styles.cards}
+        >
+          <Card style={styles.card}>
+            <Card.Content style={styles.cardContent}>
+              <Icon
+                name="Trophy"
+                style={styles.icon}
+                size={30}
+                color={primary}
+              />
+              <Paragraph>Current Streak</Paragraph>
+              <Title>
+                {streak} day{streak === 1 ? "" : "s"}
+              </Title>
+            </Card.Content>
+          </Card>
+          <Card style={styles.card}>
+            <Card.Content style={styles.cardContent}>
+              <Icon
+                name="calendar"
+                style={styles.icon}
+                size={30}
+                color={primary}
+              />
+              <Paragraph>Total Sessions</Paragraph>
+              <Title>
+                {totalSessions} session{totalSessions === 1 ? "" : "s"}
+              </Title>
+            </Card.Content>
+          </Card>
+          <Card style={styles.card}>
+            <Card.Content style={styles.cardContent}>
+              <Icon
+                name="clockcircleo"
+                style={styles.icon}
+                size={30}
+                color={primary}
+              />
+              <Paragraph>Time Meditating</Paragraph>
+              <Title>{listenedStat}</Title>
+            </Card.Content>
+          </Card>
+        </ScrollView>
+        <Calendar markedDates={markedDates} />
+        <View style={styles.quoteContainer}>
+          <Card style={styles.quoteCard}>
+            <Card.Content style={styles.cardContent}>
+              <Paragraph>{quote.author}</Paragraph>
+              <Title style={styles.quoteTitle}>{quote.quote}</Title>
+            </Card.Content>
+          </Card>
         </View>
-    );
+      </Screen>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  cards: {
+    marginBottom: 30,
+  },
+  card: {
+    width: 150,
+    marginRight: 10,
+    textAlign: "center",
+  },
+  quoteContainer: { marginRight: 10, marginBottom: 30 },
+  quoteCard: {
+    width: "100%",
+  },
+  quoteTitle: {
+    textAlign: "center",
+  },
+  cardContent: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  icon: {
+    marginBottom: 10,
+  },
+});
